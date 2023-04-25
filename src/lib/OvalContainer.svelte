c300f9d7062efd08c4c608370941eeeb6c61597b
Nikola Ganchev <nikolamihailovg@gmail.com>
2023-04-25 12:51:09 +0300

    Add hello section

---
+++ src/lib/OvalContainer.svelte
@@ -0,0 +1,52 @@
+<script>
+	export let backgroundColor = "#FFFFFF";
+	export let includeTop = true;
+	export let includeBottom = true;
+</script>
+
+<div class="container" style="--background-color: {backgroundColor}; --top-display: {includeTop ? 'block': 'none'}; --bottom-display: {includeBottom ? 'block': 'none'};">
+	<div class="content">
+		<slot />
+	</div>
+</div>
+
+<style>
+	.container {
+		background-color: var(--background-color);
+		width: 100%;
+		position: relative;
+        overflow-x: clip;
+	}
+
+	.container:before {
+		content: "";
+		width: 106%;
+		left: 50%;
+		margin-left: -53%;
+        height: 8rem;
+        border-radius: 50%;
+        background-color: white;
+		position: absolute;
+		top: -4rem;
+		display: var(--top-display);
+	}
+
+	.container:after {
+        content: "";
+        width: 106%;
+        left: 50%;
+        margin-left: -53%;
+        height: 8rem;
+        border-radius: 50%;
+        background-color: #FFFFFF;
+        position: absolute;
+       	bottom: -4rem;
+        display: var(--bottom-display);
+	}
+
+	.content {
+		position: relative;
+		padding: 4rem;
+		z-index: 2;
+	}
+</style>
