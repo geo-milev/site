2c30865ffdda7fc82bcad6f64c99ace0efdff204
Nikola Ganchev <nikolamihailovg@gmail.com>
2023-04-30 21:53:40 +0300

    Add what we study section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,6 +31,13 @@
 	</div>
 </OvalContainer>
 
+<OvalContainer>
+	<div class="what-we-study-section">
+		<h3>Какво се учи в ППМГ “Гео Милев”</h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velзit esse cillum dolore eu fugiat nulla pariatur.</p>
+	</div>
+</OvalContainer>
+
 <style>
 	.hello-section {
 		width: 100%;
@@ -69,4 +76,38 @@
 		padding-top: 2rem;
         color: #000000
 	}
+
+    .what-we-study-section {
+		width: 100%;
+        display: flex;
+        justify-content: center;
+		align-items: center;
+		flex-direction: column;
+    }
+
+	.what-we-study-section h3 {
+        font-family: 'Alegreya', serif;
+        font-style: normal;
+        font-weight: 700;
+        font-size: 48px;
+        line-height: 65px;
+        text-align: center;
+        color: #000000;
+		max-width: 30rem;
+		margin-top: 0;
+		margin-bottom: 1rem;
+    }
+
+    .what-we-study-section p {
+        font-family: 'Roboto', serif;
+        font-style: normal;
+        font-weight: 300;
+        font-size: 15px;
+        line-height: 18px;
+        text-align: center;
+        width: calc(1200px - 50vw);
+		max-width: 100%;
+		min-width: 40%;
+        color: #000000;
+    }
 </style>
