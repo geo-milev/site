a4ab2dbe2fa2ed4ccc815eaab7a11ec7b4816c33
Nikola Ganchev <nikolamihailovg@gmail.com>
2023-05-27 22:25:42 +0300

    Add projects page

--- src/lib/SecondaryButton.svelte
+++ src/lib/SecondaryButton.svelte
@@ -4,9 +4,10 @@
 	export let href: string = undefined;
 	export let action: () => void = undefined;
 	export let text: string;
+	export let color = "#FFFFFF";
 </script>
 
-<div class="content">
+<div class="content" style="--color: {color}">
 	<Button href="{href}" action="{action}" text="{text}" />
 </div>
 
@@ -14,9 +15,10 @@
 <style>
     .content :global(button) {
         background-color: rgba(0, 0, 0, 0);
-        border: 2px solid #FFFFFF;
+        border: 2px solid var(--color);
         font-weight: 300;
         font-size: 19px;
         line-height: 22px;
+		color: var(--color);
     }
 </style>
