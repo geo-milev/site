5c34ceb2e1a97bc9497d621313f368fd2d2e6133
Nikola Ganchev <nikolamihailovg@gmail.com>
2023-05-29 14:07:52 +0300

    Add mobile navigation bar

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,4 +1,9 @@
 <script lang="ts">
+    import NavLines from "$lib/NavLines.svelte";
+    import NavClose from "$lib/NavClose.svelte";
+    import NavMinus from "$lib/NavMinus.svelte";
+    import NavPlus from "$lib/NavPlus.svelte";
+
     interface NavigationLink {
         key: string;
         href?: string;
@@ -6,6 +11,7 @@
     }
 
     let shownSubsectionsHref = ""
+    let mobileOpenSubsectionsKeys = []
 
     const navigationLinksLeft: NavigationLink[] = [
         { key: "Новини", href: "/news" },
@@ -31,9 +37,20 @@
         { key: "Контакти", href: "/contacts" },
     ];
 
+    const mobileBreakpoint = 1050;
+
     let scrollY = 0;
     let leftNavWidth;
     let rightNavWidth;
+    let innerWidth;
+    let isMobileMenuOpen;
+    let navBackground;
+
+    const onBackgroundClick = (event) => {
+        if (event.target == navBackground) isMobileMenuOpen = false;
+    }
+
+    $: isMobile = innerWidth < mobileBreakpoint;
 
     // By default, the navbar is a part of the normal page to prevent content colliding with it
     // This prop allows it to be set to fixed in cases such as the main page, where the content
@@ -48,67 +65,81 @@
     $: scrollMode = (scrollY > 0) || !fixed;
 </script>
 
-<svelte:window bind:scrollY="{scrollY}" />
+<svelte:window bind:scrollY="{scrollY}" bind:innerWidth />
 
-<div class="nav-container" class:fixed="{fixed}" style="--logo-width: {logoWidth}px">
+<div class="nav-container" class:fixed="{fixed}" style="--logo-width: {logoWidth}px; --mobile-breakpoint: {mobileBreakpoint}px">
     <div class="navbar">
         <div class="background" class:scrolled="{scrollMode}"></div>
 
-        <nav class:scrolled="{scrollMode}" class="left-nav" bind:clientWidth={leftNavWidth}>
-            {#each navigationLinksLeft as navigationLink}
-                {#if !navigationLink.subsections}
-                    <a href="{navigationLink.href}">{navigationLink.key}</a>
-                    {:else}
-                    <div on:mouseenter={() => { shownSubsectionsHref = navigationLink.key }}
-                         on:mouseleave="{() => { shownSubsectionsHref = '' }}"
-                         class="subsection-container">
-                        <span>{navigationLink.key}</span>
-                        {#if shownSubsectionsHref === navigationLink.key}
-                            <div class="subsections" style="--subsection-width: {leftNavWidth + 'px'}">
-                                <div class="subsection-top">
-                                    <div class="subsection-line"></div>
-                                    <span class="subsection-title">{navigationLink.key}</span>
+        {#if !isMobile}
+            <nav class:scrolled="{scrollMode}" class="left-nav" bind:clientWidth={leftNavWidth}>
+                {#each navigationLinksLeft as navigationLink}
+                    {#if !navigationLink.subsections}
+                        <a href="{navigationLink.href}">{navigationLink.key}</a>
+                        {:else}
+                        <div on:mouseenter={() => { shownSubsectionsHref = navigationLink.key }}
+                             on:mouseleave="{() => { shownSubsectionsHref = '' }}"
+                             class="subsection-container">
+                            <span>{navigationLink.key}</span>
+                            {#if shownSubsectionsHref === navigationLink.key}
+                                <div class="subsections" style="--subsection-width: {leftNavWidth + 'px'}">
+                                    <div class="subsection-top">
+                                        <div class="subsection-line"></div>
+                                        <span class="subsection-title">{navigationLink.key}</span>
+                                    </div>
+                                    {#each navigationLink.subsections as subsection}
+                                        <a href="{subsection.href}">{subsection.key}</a>
+                                     {/each}
                                 </div>
-                                {#each navigationLink.subsections as subsection}
-                                    <a href="{subsection.href}">{subsection.key}</a>
-                                 {/each}
-                            </div>
-                        {/if}
-                    </div>
-                {/if}
-            {/each}
-        </nav>
-
+                            {/if}
+                        </div>
+                    {/if}
+                {/each}
+            </nav>
+        {:else}
+            <div class="left-nav"></div>
+        {/if}
         <div class="logo">
             <a href="/">
                 <img alt="{logoHrefAlt}" src="{logoHref}"/>
             </a>
         </div>
 
-        <nav class:scrolled="{scrollMode}" class="right-nav" bind:clientWidth={rightNavWidth}>
-            {#each navigationLinksRight as navigationLink}
-                {#if !navigationLink.subsections}
-                    <a href="{navigationLink.href}">{navigationLink.key}</a>
-                {:else}
-                    <div on:mouseenter={() => { shownSubsectionsHref = navigationLink.href }}
-                         on:mouseleave="{() => { shownSubsectionsHref = '' }}"
-                         class="subsection-container">
-                        <span>{navigationLink.key}</span>
-                        {#if shownSubsectionsHref === navigationLink.href}
-                            <div class="subsections" style="--subsection-width: {rightNavWidth + 'px'}">
-                                <div class="subsection-top">
-                                    <div class="subsection-line"></div>
-                                    <span class="subsection-title">{navigationLink.key}</span>
+        {#if !isMobile}
+            <nav class:scrolled="{scrollMode}" class="right-nav" bind:clientWidth={rightNavWidth}>
+                {#each navigationLinksRight as navigationLink}
+                    {#if !navigationLink.subsections}
+                        <a href="{navigationLink.href}">{navigationLink.key}</a>
+                    {:else}
+                        <div on:mouseenter={() => { shownSubsectionsHref = navigationLink.href }}
+                             on:mouseleave="{() => { shownSubsectionsHref = '' }}"
+                             class="subsection-container">
+                            <span>{navigationLink.key}</span>
+                            {#if shownSubsectionsHref === navigationLink.href}
+                                <div class="subsections" style="--subsection-width: {rightNavWidth + 'px'}">
+                                    <div class="subsection-top">
+                                        <div class="subsection-line"></div>
+                                        <span class="subsection-title">{navigationLink.key}</span>
+                                    </div>
+                                    {#each navigationLink.subsections as subsection}
+                                        <a href="{subsection.href}">{subsection.key}</a>
+                                    {/each}
                                 </div>
-                                {#each navigationLink.subsections as subsection}
-                                    <a href="{subsection.href}">{subsection.key}</a>
-                                {/each}
-                            </div>
-                        {/if}
-                    </div>
-                {/if}
-            {/each}
-        </nav>
+                            {/if}
+                        </div>
+                    {/if}
+                {/each}
+            </nav>
+            {:else}
+            <div class="right-nav" class:scrolled="{scrollMode}" >
+                <button class="icon-button" on:click={() => {
+                    isMobileMenuOpen = true
+                    document.body.classList.add("no-scroll")
+                }}>
+                    <NavLines></NavLines>
+                </button>
+            </div>
+        {/if}
     </div>
     {#if showTitle && fixed}
         <h1 class:scrolled="{scrollMode}">{title}</h1>
@@ -117,10 +148,56 @@
             <h1>{title}</h1>
         {/if}
     {/if}
+
+    {#if isMobile && isMobileMenuOpen}
+        <div class="nav-background" on:click={onBackgroundClick} bind:this={navBackground}></div>
+        <div class="mobile-nav">
+            <div class="close">
+                <button class="icon-button" on:click={() => { isMobileMenuOpen = false }}>
+                    <NavClose />
+                </button>
+            </div>
+            <nav>
+                {#each [...navigationLinksLeft, ...navigationLinksRight] as navigationLink}
+                    {#if navigationLink.href}
+                        <div class="link-wrapper">
+                            <a href="{navigationLink.href}">{navigationLink.key}</a>
+                        </div>
+                    {:else}
+                        <button class="link-wrapper" on:click={() => {
+                            if (mobileOpenSubsectionsKeys.includes(navigationLink.key)) {
+                                mobileOpenSubsectionsKeys = mobileOpenSubsectionsKeys
+                                    .filter((val) => val !== navigationLink.key)
+                            } else {
+                                mobileOpenSubsectionsKeys = [...mobileOpenSubsectionsKeys, navigationLink.key]
+                            }
+                        }}>
+                            <span>{navigationLink.key}</span>
+                            {#if mobileOpenSubsectionsKeys.includes(navigationLink.key)}
+                                <NavMinus />
+                                {:else}
+                                <NavPlus />
+                            {/if}
+                        </button>
+                        {#if mobileOpenSubsectionsKeys.includes(navigationLink.key)}
+                            <div class="mobile-subsection">
+                                {#each navigationLink.subsections as subsection}
+                                    <a href="{subsection.href}">{subsection.key}</a>
+                                {/each}
+                            </div>
+                        {/if}
+                    {/if}
+                {/each}
+            </nav>
+        </div>
+    {/if}
 </div>
 
-
 <style>
+    :global(.noScroll) {
+        overflow: hidden;
+    }
+
     .nav-container {
         position: sticky;
         top: 0;
@@ -282,5 +359,96 @@
         text-transform: uppercase;
         text-align: center;
         margin: 0;
+    }
+
+    .nav-background {
+        width: 100%;
+        height: 100%;
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        z-index: 4;
+        pointer-events: all;
+    }
+
+    .close {
+        display: flex;
+        justify-content: flex-end;
+        padding: 1rem;
+    }
+
+    .mobile-nav {
+        height: 100%;
+        position: fixed;
+        min-width: 50%;
+        right: 0;
+        top: 0;
+        background-color: #4F0D0D;
+        z-index: 5;
+    }
+
+    .mobile-nav nav {
+        display: flex;
+        width: 100%;
+        height: 100%;
+        flex-direction: column;
+        border: none;
+        align-items: flex-start;
+        justify-content: flex-start;
+        padding: 0 2rem 1rem 1rem;
+        gap: 10px;
+        box-sizing: border-box;
+    }
+
+
+    .mobile-nav nav a, .mobile-nav nav span {
+        margin-top: 0;
+    }
+
+    .link-wrapper {
+        border-bottom: 1px solid #FFFFFF;
+        padding: 0 0 5px;
+        width: 100%;
+        justify-content: space-between;
+        align-items: center;
+        display: flex;
+        background-color: rgba(0, 0, 0, 0);
+        border-top: none;
+        border-left: none;
+        border-right: none;
+        font-size: 16px;
+        cursor: pointer;
+    }
+
+    .mobile-subsection {
+        padding-top: 4px;
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+    }
+
+    .mobile-subsection a {
+        font-family: 'Roboto', serif;
+        font-style: normal;
+        font-weight: 300;
+        font-size: 15px;
+        line-height: 17px;
+        text-transform: none;
+        color: #FFFFFF;
+    }
+
+    .icon-button {
+        background-color: rgba(0, 0, 0, 0);
+        border: none;
+        cursor: pointer;
+        pointer-events: all;
+    }
+
+    button {
+        transition: margin-bottom 250ms;
+    }
+
+    .scrolled button {
+        margin-bottom: 1.5rem;
     }
 </style>
