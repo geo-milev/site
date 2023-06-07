31c6f1002da34fc6abfa7f7ea67801ab40663556
Nikola Ganchev <nikolamihailovg@gmail.com>
2023-06-07 20:31:48 +0300

    Add proper news not found message

--- src/routes/news/[id]/+page.js
+++ src/routes/news/[id]/+page.js
@@ -2,15 +2,13 @@ import { env } from "$env/dynamic/public";
 import { error } from "@sveltejs/kit";
 
 export async function load({ fetch, params }) {
-    const res = (
-        await fetch(env.PUBLIC_SERVER_URL + `/api/news/${params.id}`)
-    ).json();
+    const res = await fetch(env.PUBLIC_SERVER_URL + `/api/news/${params.id}`);
 
-    if (!res) {
+    if (res.status === 404) {
         throw error(404);
     }
 
     return {
-        News: res,
+        News: await res.json(),
     };
 }
