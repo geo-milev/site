e43a7a13ad4f9612806a1bb73118e6ec1af5792a
Nikola Ganchev <nikolamihailovg@gmail.com>
2023-06-19 10:44:20 +0300

    Add exercises page

---
+++ src/routes/admission/exercises/+page.js
@@ -0,0 +1,27 @@
+import { cacheExchange, Client, fetchExchange } from "@urql/svelte";
+import { env } from "$env/dynamic/public";
+
+export async function load({ fetch }) {
+    const client = new Client({
+        url: env.PUBLIC_SERVER_URL + "/api/graphql",
+        exchanges: [cacheExchange, fetchExchange],
+        fetch: fetch,
+    });
+
+    const QUERY = `
+     	query($page: Int!) {
+            Exercises(page: $page, limit: 15) {
+                docs {
+                    name
+                    file {
+                        url
+                        filename
+                    }
+                }
+                hasNextPage
+            }
+        }
+    `;
+
+    return (await client.query(QUERY, { page: 1 })).data;
+}
