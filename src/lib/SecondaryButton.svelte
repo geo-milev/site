aa82af09f1295ed58b05a9dfff1a442b5d3fa443
Nikola Ganchev <nikolamihailovg@gmail.com>
2023-09-27 21:36:03 +0300

    Remove SecondaryButton variable colors

--- src/lib/SecondaryButton.svelte
+++ src/lib/SecondaryButton.svelte
@@ -4,15 +4,10 @@
 	export let href: string = undefined;
 	export let action: () => void = undefined;
 	export let text: string;
-	export let color = "#FFFFFF";
-	export let hoverColor = "#FFFFFF"
-	export let hoverTextColor = "#000000"
 	export let active = false;
 </script>
 
-<div class="content-button" style="--color: {color};
---hover-color: {hoverColor};
---hover-text-color: {hoverTextColor}" class:active={active}>
+<div class="content-button" class:active={active}>
 	<Button href="{href}" action="{action}" text="{text}" />
 </div>
 
@@ -20,25 +15,25 @@
 <style>
     .content-button :global(button) {
         background-color: rgba(0, 0, 0, 0);
-        border: 2px solid var(--color);
+        border: 2px solid #FFFFFF;
         font-weight: 300;
         font-size: 19px;
         line-height: 22px;
-		color: var(--color);
+		color: #FFFFFF;
         font-family: 'Roboto Flex Variable', serif;
         font-variation-settings: 'GRAD' -150;
     }
 
     .content-button :global(button:hover) {
-        color: var(--hover-text-color);
-        background-color: var(--hover-color);
-        border: 2px solid var(--hover-color);
+        color: #000000;
+        background-color: #FFFFFF;
+        border: 2px solid #FFFFFF;
         font-variation-settings: 'GRAD' 150;
     }
     .content-button.active :global(button) {
-        color: var(--hover-text-color);
-        background-color: var(--hover-color);
-        border: 2px solid var(--hover-color);
+        color: #000000;
+        background-color: #FFFFFF;
+        border: 2px solid #FFFFFF;
         font-variation-settings: 'GRAD' 150;
     }
 
