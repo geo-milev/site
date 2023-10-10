3f749cb07f68a3695b3ca4f5f7272e0e70cd0806
Nikola Ganchev <nikolamihailovg@gmail.com>
2023-10-10 21:15:30 +0300

    Fix budgets using old graphql name

--- src/routes/organisation/budget/+page.js
+++ src/routes/organisation/budget/+page.js
@@ -10,7 +10,7 @@ export async function load({ fetch }) {
 
     const QUERY = `
      	query {
-            AvailableBudget {
+            Budget {
                 years {
                     year
                     budgets {
